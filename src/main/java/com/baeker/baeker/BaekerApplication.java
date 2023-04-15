579b90d18515ea9b596c1aa549eff0c27a22e47a
PARKPARKWOO <wy9295@naver.com>
2023-04-15 21:25:16 +0900

    feat: scheduling

--- src/main/java/com/baeker/baeker/BaekerApplication.java
+++ src/main/java/com/baeker/baeker/BaekerApplication.java
@@ -2,8 +2,10 @@ package com.baeker.baeker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class BaekerApplication {
 
 	public static void main(String[] args) {
