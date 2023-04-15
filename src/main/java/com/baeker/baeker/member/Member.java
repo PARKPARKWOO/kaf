05f7561b6fe45ea31b9069b2b16bfd7f74d0d304
choideakook <shdrnrhd113@naver.com>
2023-04-15 22:15:38 +0900

    feat: login 기능 구현

--- src/main/java/com/baeker/baeker/member/Member.java
+++ src/main/java/com/baeker/baeker/member/Member.java
@@ -5,8 +5,12 @@ import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 import static jakarta.persistence.GenerationType.IDENTITY;
 import static lombok.AccessLevel.PROTECTED;
@@ -22,7 +26,7 @@ public class Member {
     @Id
     @GeneratedValue(strategy = IDENTITY)
     private Long id;
-
+    @Column(unique = true)
     private String userId;
     private String name;
     private String password;
@@ -36,15 +40,25 @@ public class Member {
 
 
     //-- crate method --//
-    protected static Member createMember(String userId, String name, String password, String provider, String token) {
+    protected static Member createMember(String userId, String name, String password) {
         return builder()
                 .userId(userId)
                 .name(name)
                 .password(password)
-                .provider(provider)
-                .token(token)
                 .build();
     }
 
+    //-- create authorize --//
+    public List<? extends GrantedAuthority> getGrantedAuthorities() {
+        List<GrantedAuthority> grantedAuthorities = new ArrayList<>();
 
+        // 모든 회원에게 member 권한 부여 //
+        grantedAuthorities.add(new SimpleGrantedAuthority("member"));
+
+        // admin 권한 부여 //
+        if ("admin".equals(userId))
+            grantedAuthorities.add(new SimpleGrantedAuthority("admin"));
+
+        return grantedAuthorities;
+    }
 }
---
+++ src/main/java/com/baeker/baeker/member/form/MemberJoinForm.java
@@ -0,0 +1,25 @@
+package com.baeker.baeker.member.form;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class MemberJoinForm {
+
+    @NotBlank
+    @Size(min = 4, max = 20)
+    private String userId;
+    @NotBlank
+    @Size(min = 2, max = 10)
+    private String name;
+
+    @NotBlank
+    @Size(min = 4, max = 20)
+    private String password;
+    @NotBlank
+    @Size(min = 4, max = 20)
+    private String password2;
+}
