1a4386805d15d6ed06905cfda5930bc119dc7b56
PARKPARKWOO <wy9295@naver.com>
2023-04-15 17:26:16 +0900

    setting

---
+++ src/main/java/com/baeker/baeker/BaekerApplication.java
@@ -0,0 +1,13 @@
+package com.baeker.baeker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaekerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaekerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/baeker/baeker/BaekerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baeker.baeker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaekerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
