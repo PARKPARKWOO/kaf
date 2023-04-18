0fadf316f1b545f32ffc46be99eb2fda9629c75e
choideakook <shdrnrhd113@naver.com>
2023-04-18 11:22:04 +0900

    design: member profile ui 수정

--- src/main/java/com/baeker/baeker/base/initDB/InitDB.java
+++ src/main/java/com/baeker/baeker/base/initDB/InitDB.java
@@ -3,6 +3,8 @@ package com.baeker.baeker.base.initDB;
 import com.baeker.baeker.member.Member;
 import com.baeker.baeker.member.MemberService;
 import com.baeker.baeker.member.form.MemberJoinForm;
+import com.baeker.baeker.myStudy.MyStudy;
+import com.baeker.baeker.myStudy.MyStudyService;
 import com.baeker.baeker.study.Study;
 import com.baeker.baeker.study.StudyService;
 import com.baeker.baeker.study.form.StudyCreateForm;
@@ -12,7 +14,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
-//@Configuration
+@Configuration
 @RequiredArgsConstructor
 public class InitDB {
 
@@ -30,13 +32,24 @@ public class InitDB {
 
         private final MemberService memberService;
         private final StudyService studyService;
+        private final MyStudyService myStudyService;
 
         public void initData() {
+
+            // 맴버 "알파" 생성
             MemberJoinForm memberForm = new MemberJoinForm("user1", "알파", "안녕하세요 알파입니다.", "1234", "1234");
             Member member = memberService.join(memberForm).getData();
 
-            StudyCreateForm studyFrom = new StudyCreateForm("Baeker lover", "Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.", 10);
+            // 알파가 스터디 "Baeker lover" 생성
+            StudyCreateForm studyFrom = new StudyCreateForm("Baeker lover", "Provident cupiditate voluptatem et in.", 10);
             Study study = studyService.create(studyFrom, member).getData();
+
+            // init 맴버 8명 생성후 "Baeker lover" 에 가입
+            for (int i = 2; i < 10; i++) {
+                Member memberA = memberService.join(new MemberJoinForm("user" + i, "member" + i, "hello", "1234", "1234")).getData();
+                MyStudy myStudy = myStudyService.join(memberA, study).getData();
+                myStudyService.accept(myStudy);
+            }
         }
 
     }
