67320e065d4f918ebc758525fdb2ebce3e8b4bae
choideakook <shdrnrhd113@naver.com>
2023-04-18 15:25:32 +0900

    test: 해결한 문제 로직 테스트 성공

--- src/main/java/com/baeker/baeker/member/Member.java
+++ src/main/java/com/baeker/baeker/member/Member.java
@@ -56,6 +56,7 @@ public class Member {
                 .name(name)
                 .about(about)
                 .password(password)
+                .solvedCount(0)
                 .build();
     }
 
