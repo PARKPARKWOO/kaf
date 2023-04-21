f31ad59bd4f106ecd1ea24ab8ebec09d0890a051
choideakook <shdrnrhd113@naver.com>
2023-04-21 22:59:36 +0900

    feat: 초대 기능 Controller 까지 구현 (html 구현 x)

--- src/main/java/com/baeker/baeker/myStudy/MyStudyController.java
+++ src/main/java/com/baeker/baeker/myStudy/MyStudyController.java
@@ -1,9 +1,18 @@
 package com.baeker.baeker.myStudy;
 
 import com.baeker.baeker.base.request.Rq;
+import com.baeker.baeker.base.request.RsData;
+import com.baeker.baeker.member.Member;
+import com.baeker.baeker.member.MemberService;
+import com.baeker.baeker.myStudy.form.MyStudyInviteForm;
+import com.baeker.baeker.study.Study;
+import com.baeker.baeker.study.StudyService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Slf4j
@@ -13,9 +22,42 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class MyStudyController {
 
     private final MyStudyService myStudyService;
+    private final MemberService memberService;
+    private final StudyService studyService;
     private final Rq rq;
 
     //-- 가입 신청하기 --//
 
     //-- 스터디로 초대하기 --//
+    @PostMapping("invite/{id}")
+    @PreAuthorize("isAuthenticated()")
+    public String invite(
+            @PathVariable Long id,
+            MyStudyInviteForm form
+    ) {
+        Member inviter = rq.getMember();
+        log.info("스터디 초대 요청 확인 inviter id = {}", inviter.getId());
+
+        RsData<Member> inviteeRs = memberService.getMember(id);
+        if (inviteeRs.isFail()) {
+            log.info("invitee 조회 실패 error = {}", inviteeRs.getMsg());
+            return rq.historyBack(inviteeRs.getMsg());
+        }
+
+        RsData<Study> studyRs = studyService.getStudy(form.getStudyId());
+        if (studyRs.isFail()) {
+            log.info("study 조회 실패 error = {}", studyRs.getMsg());
+            return rq.historyBack(studyRs.getMsg());
+        }
+
+        RsData<MyStudy> inviteRs
+                = myStudyService.invite(inviter, inviteeRs.getData(), studyRs.getData(), form.getMsg());
+
+        if (inviteeRs.isFail()) {
+            log.info("초대 실패 error = {}", inviteRs.getMsg());
+            return rq.historyBack(inviteRs.getMsg());
+        }
+
+        return rq.redirectWithMsg("/study/detail/" + studyRs.getData().getId(), inviteRs.getMsg());
+    }
 }
