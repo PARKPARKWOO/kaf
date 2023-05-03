c8cf4c3ffa1f92d32fe81df7a2b92564734a5d10
choideakook <shdrnrhd113@naver.com>
2023-05-03 23:44:19 +0900

    design: 백준 스냅샷 차트 ui 구현

--- src/main/java/com/baeker/baeker/solvedApi/ApiScheduler.java
+++ src/main/java/com/baeker/baeker/solvedApi/ApiScheduler.java
@@ -29,7 +29,7 @@ public class ApiScheduler {
      * 티어 별 check
      */
 
-    @Scheduled(fixedRate = 1000)
+//    @Scheduled(fixedRate = 1000)
     public void checkStudyRule() throws IOException, ParseException {
         log.info("스케줄러 실행");
         RsData<List<Member>> memberList = memberService.getAll();
